6e3f9c6ec8739042830870f135d7e10d9633dd08
Georgena Georgieva <georgena1991@gmail.com>
2021-02-07 17:59:39 +0200

    Navbar Template

---
+++ ReactApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className='welcome'> Welcome!</h1>
+    </div>
+  );
+}
+
+export default App;
